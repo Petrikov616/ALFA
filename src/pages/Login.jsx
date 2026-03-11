df802206c8285b600e02aa70c732c215d2953847
Petrikov616 <hincapiezuleta813@gmail.com>
2026-03-11 18:08:35 -0500

    Se modificaron archivos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,47 @@
-import "./Login.css";
-import escudo from "../assets/escudo.png";
-import logo from "../assets/logoQR.png";
+    import "./Login.css";
+    import escudo from "../assets/escudo.png";
+    import logo from "../assets/logoQR.png";
 
-function Login() {
-    return (
-        <section className="login">
-            <div className="logo-header">
-                <img src={logo} alt="logoQR" className="logo" />
-                <span className="brand">AgilCheck</span>
-            </div>
-
-            <div className="login-card">
-                <div className="login-left">
-                    <img src={escudo} alt="Escudo institucional" className="login-image" />
+    function Login() {
+        return (
+            <section className="login">
+                <div className="logo-header">
+                    <img src={logo} alt="logoQR" className="logo" />
+                    <span className="brand">AgilCheck</span>
                 </div>
 
-                <div className="login-right">
-                    <h2>Iniciar sesion</h2>
-                    <p>Bienvenido al sistema AgilCheck</p>
+                <div className="login-card">
+                    <div className="login-left">
+                        <img src={escudo} alt="Escudo institucional" className="login-image" />
+                    </div>
 
-                    <form className="login-form">
+                    <div className="login-right">
+                        <h2>Iniciar sesion</h2>
+                        <p>Bienvenido al sistema AgilCheck</p>
 
-                        <div className="input-group">
-                            <label>Usuario</label>
-                            <input type="text" placeholder="Ingrese su usuario" />
-                        </div>
+                        <form className="login-form">
 
-                        <div className="input-group">
-                            <label>Contraseña</label>
-                            <input type="password" placeholder="Ingrese su contraseña" />
-                        </div>
+                            <div className="input-group">
+                                <label>Usuario</label>
+                                <input type="text" placeholder="Ingrese su usuario" />
+                            </div>
 
-                        <div className="forgot-password">
-                            <a href="#">¿Olvidaste tu contraseña?</a>
-                        </div>
+                            <div className="input-group">
+                                <label>Contraseña</label>
+                                <input type="password" placeholder="Ingrese su contraseña" />
+                            </div>
 
-                        <button type="submit" className="login-boton">Ingresar</button>
-                    </form>
+                            <div className="forgot-password">
+                                <a href="#">¿Olvidaste tu contraseña?</a>
+                            </div>
+
+                            <button type="submit" className="login-boton">Ingresar</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
-    );
-}
+        );
+    }
 
-export default Login;
+    export default Login;
